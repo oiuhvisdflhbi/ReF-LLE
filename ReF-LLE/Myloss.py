5c5ff1c28b945f6479c126fa6ac75d8a8a6770e5
Ming Zhao <150154503+oiuhvisdflhbi@users.noreply.github.com>
2025-02-19 12:11:58 +0800

    Add files via upload

---
+++ ReF-LLE/Myloss.py
@@ -0,0 +1,240 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from torchvision.models.vgg import vgg16
+import numpy as np
+
+
+class L_color(nn.Module):
+
+    def __init__(self):
+        super(L_color, self).__init__()
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+
+        mean_rgb = torch.mean(x, [2, 3], keepdim=True)
+        mr, mg, mb = torch.split(mean_rgb, 1, dim=1)
+        Drg = torch.pow(mr - mg, 2)
+        Drb = torch.pow(mr - mb, 2)
+        Dgb = torch.pow(mb - mg, 2)
+        k = torch.pow(torch.pow(Drg, 2) + torch.pow(Drb, 2) + torch.pow(Dgb, 2), 0.5)
+
+        return k
+
+class L_area_loss(nn.Module):
+    def __init__(self):
+        super(L_area_loss, self).__init__()
+
+    def forward(self, image):
+        image = image.mean(axis=1)
+        #print(image.shape)
+        center_x = image.shape[1] // 2
+        center_y = image.shape[2] // 2
+
+        area1 = image[:, center_y-25:center_y+25, center_x-25:center_x+25]
+        area2 = image[:, center_y-80:center_y+80, center_x-80:center_x+80]
+        area3 = image
+        #print(area1.shape)
+        area1_n = area1.size(1)*area1.size(2)
+        area2_n = area2.size(1)*area2.size(2)
+        area3_n = area3.size(1)*area3.size(2)
+        #print(area1_n.shape)
+        #print(area1_n)
+        area_a_mean = area1.mean(dim=(1,2))
+        area_b_mean = (area2.sum(dim=(1,2)) - area1.sum(dim=(1,2))) / (area2_n - area1_n)
+        area_c_mean = (area3.sum(dim=(1,2)) - area2.sum(dim=(1,2))) / (area3_n - area2_n)
+        #print("Mean of Area 1:", area_a_mean)
+        #print("Mean of Area 2:", area_b_mean)
+        #print("Mean of Area 3:", area_c_mean)
+        #loss = np.abs(6000000/image.max()-1)
+        #loss = 0.2 * np.exp(np.abs(20000 / np.maximum(area_a_mean, 600) - 1)) + 0.6 * np.exp(np.abs(2500 / np.maximum(area_b_mean, 200) - 1)) + 0.2 * np.exp(np.abs(1200 / np.maximum(area_c_mean, 150) - 1))
+        #loss = np.log(loss)
+        loss = 0.6 * np.abs(20000 / area_a_mean - 1) + 0.2 * np.abs(2500 / area_b_mean - 1) + 0.2 * np.abs(1200 / area_c_mean - 1)
+        return loss.mean()
+class L_point_loss(nn.Module):
+    def __init__(self):
+        super(L_point_loss, self).__init__()
+
+    def forward(self, image):
+        image = image.mean(axis=1)
+        #print(image.shape)
+        center_x = image.shape[1] // 2
+        center_y = image.shape[2] // 2
+        exp_point = image[:, center_x, center_y]
+        loss = np.abs(5000000 / exp_point - 1)
+        #loss = 0.6 * np.abs(20000 / area_a_mean - 1) + 0.2 * np.abs(2500 / area_b_mean - 1) + 0.2 * np.abs(1200 / area_c_mean - 1)
+        return loss.mean()        
+class L_area_measure(nn.Module):
+    def __init__(self):
+        super(L_area_measure, self).__init__()
+
+    def forward(self, image, image_ref):
+        
+        loss = np.abs(image.max()/image_ref - 1)
+        
+        return loss
+               
+class L_color_rate(nn.Module):
+
+    def __init__(self):
+        super(L_color_rate, self).__init__()
+
+    def forward(self, pre, cur):
+        b, c, h, w = pre.shape
+
+        # mean_rgb_pre = torch.mean(pre, [2, 3], keepdim=True)
+        mr_pre, mg_pre, mb_pre = torch.split(pre*255, 1, dim=1)
+        # mean_rgb_cur = torch.mean(cur, [2, 3], keepdim=True)
+        mr_cur, mg_cur, mb_cur = torch.split(cur*255, 1, dim=1)
+
+        Drg = torch.pow(mr_pre.int()//mg_pre.int() - mr_cur.int()//mg_cur.int(), 2).sum()/255.0**2
+        Drb = torch.pow(mr_pre.int()//mb_pre.int() - mr_cur.int()//mb_cur.int(), 2).sum()/255.0**2
+        Dgb = torch.pow(mg_pre.int()//mb_pre.int() - mg_cur.int()//mb_cur.int(), 2).sum()/255.0**2
+
+        # Drg_pre = torch.pow(mr_pre - mg_pre, 2)
+        # Drb_pre = torch.pow(mr_pre - mb_pre, 2)
+        # Dgb = torch.pow(mb_pre - mg_pre, 2)
+        k = torch.pow(Drg + Drb + Dgb, 0.5)
+
+        return k
+
+class L_spa(nn.Module):
+
+    def __init__(self):
+        super(L_spa, self).__init__()
+        # print(1)kernel = torch.FloatTensor(kernel).unsqueeze(0).unsqueeze(0)
+        kernel_left = torch.FloatTensor([[0, 0, 0], [-1, 1, 0], [0, 0, 0]]).cuda().unsqueeze(0).unsqueeze(0)
+        kernel_right = torch.FloatTensor([[0, 0, 0], [0, 1, -1], [0, 0, 0]]).cuda().unsqueeze(0).unsqueeze(0)
+        kernel_up = torch.FloatTensor([[0, -1, 0], [0, 1, 0], [0, 0, 0]]).cuda().unsqueeze(0).unsqueeze(0)
+        kernel_down = torch.FloatTensor([[0, 0, 0], [0, 1, 0], [0, -1, 0]]).cuda().unsqueeze(0).unsqueeze(0)
+        self.weight_left = nn.Parameter(data=kernel_left, requires_grad=False)
+        self.weight_right = nn.Parameter(data=kernel_right, requires_grad=False)
+        self.weight_up = nn.Parameter(data=kernel_up, requires_grad=False)
+        self.weight_down = nn.Parameter(data=kernel_down, requires_grad=False)
+        self.pool = nn.AvgPool2d(4)
+
+    def forward(self, org, enhance):
+        b, c, h, w = org.shape
+
+        org_mean = torch.mean(org, 1, keepdim=True)
+        enhance_mean = torch.mean(enhance, 1, keepdim=True)
+
+        org_pool = self.pool(org_mean)
+        enhance_pool = self.pool(enhance_mean)
+
+        weight_diff = torch.max(
+            torch.FloatTensor([1]).cuda() + 10000 * torch.min(org_pool - torch.FloatTensor([0.3]).cuda(),
+                                                              torch.FloatTensor([0]).cuda()),
+            torch.FloatTensor([0.5]).cuda())
+        E_1 = torch.mul(torch.sign(enhance_pool - torch.FloatTensor([0.5]).cuda()), enhance_pool - org_pool)
+
+        D_org_letf = F.conv2d(org_pool, self.weight_left, padding=1)
+        D_org_right = F.conv2d(org_pool, self.weight_right, padding=1)
+        D_org_up = F.conv2d(org_pool, self.weight_up, padding=1)
+        D_org_down = F.conv2d(org_pool, self.weight_down, padding=1)
+
+        D_enhance_letf = F.conv2d(enhance_pool, self.weight_left, padding=1)
+        D_enhance_right = F.conv2d(enhance_pool, self.weight_right, padding=1)
+        D_enhance_up = F.conv2d(enhance_pool, self.weight_up, padding=1)
+        D_enhance_down = F.conv2d(enhance_pool, self.weight_down, padding=1)
+
+        D_left = torch.pow(D_org_letf - D_enhance_letf, 2)
+        D_right = torch.pow(D_org_right - D_enhance_right, 2)
+        D_up = torch.pow(D_org_up - D_enhance_up, 2)
+        D_down = torch.pow(D_org_down - D_enhance_down, 2)
+        E = (D_left + D_right + D_up + D_down)
+        # E = 25*(D_left + D_right + D_up +D_down)
+
+        return E
+
+
+class L_exp(nn.Module):
+
+    def __init__(self, patch_size, mean_val):
+        super(L_exp, self).__init__()
+        # print(1)
+        self.pool = nn.AvgPool2d(patch_size)
+        self.mean_val = mean_val
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        x = torch.mean(x, 1, keepdim=True)
+        mean = self.pool(x)
+
+        d = torch.mean(torch.pow(mean - torch.FloatTensor([self.mean_val]).cuda(), 2))
+        return d
+
+
+class L_TV(nn.Module):
+    def __init__(self, TVLoss_weight=1):
+        super(L_TV, self).__init__()
+        self.TVLoss_weight = TVLoss_weight
+
+    def forward(self, x):
+        batch_size = x.size()[0]
+        h_x = x.size()[2]
+        w_x = x.size()[3]
+        count_h = (x.size()[2] - 1) * x.size()[3]
+        count_w = x.size()[2] * (x.size()[3] - 1)
+        h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
+        w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
+        return self.TVLoss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+
+
+class Sa_Loss(nn.Module):
+    def __init__(self):
+        super(Sa_Loss, self).__init__()
+        # print(1)
+
+    def forward(self, x):
+        # self.grad = np.ones(x.shape,dtype=np.float32)
+        b, c, h, w = x.shape
+        # x_de = x.cpu().detach().numpy()
+        r, g, b = torch.split(x, 1, dim=1)
+        mean_rgb = torch.mean(x, [2, 3], keepdim=True)
+        mr, mg, mb = torch.split(mean_rgb, 1, dim=1)
+        Dr = r - mr
+        Dg = g - mg
+        Db = b - mb
+        k = torch.pow(torch.pow(Dr, 2) + torch.pow(Db, 2) + torch.pow(Dg, 2), 0.5)
+        # print(k)
+
+        k = torch.mean(k)
+        return k
+
+
+class perception_loss(nn.Module):
+    def __init__(self):
+        super(perception_loss, self).__init__()
+        features = vgg16(pretrained=True).features
+        self.to_relu_1_2 = nn.Sequential()
+        self.to_relu_2_2 = nn.Sequential()
+        self.to_relu_3_3 = nn.Sequential()
+        self.to_relu_4_3 = nn.Sequential()
+
+        for x in range(4):
+            self.to_relu_1_2.add_module(str(x), features[x])
+        for x in range(4, 9):
+            self.to_relu_2_2.add_module(str(x), features[x])
+        for x in range(9, 16):
+            self.to_relu_3_3.add_module(str(x), features[x])
+        for x in range(16, 23):
+            self.to_relu_4_3.add_module(str(x), features[x])
+
+        # don't need the gradients, just want the features
+        for param in self.parameters():
+            param.requires_grad = False
+
+    def forward(self, x):
+        h = self.to_relu_1_2(x)
+        h_relu_1_2 = h
+        h = self.to_relu_2_2(h)
+        h_relu_2_2 = h
+        h = self.to_relu_3_3(h)
+        h_relu_3_3 = h
+        h = self.to_relu_4_3(h)
+        h_relu_4_3 = h
+        # out = (h_relu_1_2, h_relu_2_2, h_relu_3_3, h_relu_4_3)
+        return h_relu_4_3
---
+++ ReF-LLE/State_de.py
@@ -0,0 +1,137 @@
+import numpy as np
+import sys
+import cv2
+from utils import batch_psnr, normalize, init_logger_ipol, \
+				variable_to_cv2_image, remove_dataparallel_wrapper, is_rgb
+import torch
+
+import os
+from models import FFDNet
+from torch.autograd import Variable
+import matplotlib.image as mpimg
+from PIL import Image
+
+class State_de():
+    def __init__(self, size, move_range, model):
+        self.image = np.zeros(size,dtype=np.float32)
+        self.move_range = move_range
+        self.net = model
+        self.image_freq = np.zeros(size,dtype=np.float32)
+        self.image_mag = np.zeros(size,dtype=np.float32)
+        self.image_pha = np.zeros(size,dtype=np.float32)
+    def reset(self, x):
+        self.image = x
+        self.raw = x * 255
+        self.raw[np.where(self.raw <= 2)] = 3
+        self.image_freq = torch.fft.fftshift(torch.fft.fft2(torch.from_numpy(self.image*255), dim=[-2, -1], norm='backward'))
+        self.image_mag = torch.abs(self.image_freq)
+        self.image_pha = torch.angle(self.image_freq)
+        
+    def step_el(self, act):
+        #neutral = 4
+        #print(act)
+        #moves = transform_array(act.astype(np.float32)-neutral)
+        #act = np.array(act, dtype=np.float32)
+        moves = (act.astype(np.float32)-10)*0.01
+        #moves = (move - neutral) / 20
+        moves = torch.from_numpy(moves)
+        print("moves:  ",moves[:, 0, :, :].max(),moves[:, 0, :, :].min(),moves[:, 0, :, :].mean())
+        _,_,H,W = self.image.shape
+        moved_image_mag = torch.from_numpy(np.zeros(self.image.shape, dtype=np.float32))
+        # de = move[:, 3:, :, :]
+        image_mag = self.image_mag
+        #scale = image_mag.max()
+        #image_mag/=scale
+        #r = image_mag[:, 0, :, :]
+        #print("r",r.max(),r.min(),r.mean())
+        #g = image_mag[:, 1, :, :]
+        #print("g",g.max(),g.min(),g.mean())
+        #b = image_mag[:, 2, :, :]
+        #print("b",b.max(),b.min(),b.mean())
+        #moved_image_mag[:, 0, :, :] = r + (moves[:, 0, :, :]) * r * (1 - r)
+        #moved_image_mag[:, 1, :, :] = g + (0.1 * moves[:, 1, :, :] + 0.9 * moves[:, 0, :, :]) * g * (1 - g)
+        #moved_image_mag[:, 2, :, :] = b + (0.1 * moves[:, 2, :, :] + 0.9 * moves[:, 0, :, :]) * b * (1 - b)
+        #moved_image_mag[:, 0, :, :] = g + (moves[:, 1, :, :]) * g * (1 - g)
+        #moved_image_mag[:, 0, :, :] = b + (moves[:, 2, :, :]) * b * (1 - b)
+        moves[:, 0, :, :] = moves[:, 0, :, :]
+        #moves[:, 1, :, :] = 0.2 * np.exp(moves[:, 1, :, :]) + 0.8 * moves[:, 0, :, :]
+        #moves[:, 2, :, :] = 0.2 * np.exp(moves[:, 2, :, :]) + 0.8 * moves[:, 0, :, :]
+        moves[:, 1, :, :] = moves[:, 0, :, :]
+        moves[:, 2, :, :] = moves[:, 0, :, :]
+        moved_image_mag = np.exp(moves) * image_mag
+        #moved_image_mag = moves * image_mag
+        self.image_mag = moved_image_mag
+        real = moved_image_mag * torch.cos(self.image_pha)
+        imag = moved_image_mag * torch.sin(self.image_pha)
+        image_out = torch.complex(real, imag)
+        image_out = torch.fft.ifft2(torch.fft.ifftshift(image_out), s=(H, W), dim=[-2, -1], norm='backward')
+        image_out = image_out.real.numpy()
+        image_out = np.clip(image_out,0,255)
+        print(image_out.mean(),image_out.max(),image_out.min())
+        image_out = image_out/255
+        
+        self.image = 0.8 * image_out + 0.2 * self.image
+        #self.image = image_out
+
+    def step_de(self, act_b):
+        pix_num = act_b.shape[1] * act_b.shape[2]
+        threshold = pix_num
+        checker = act_b.sum(1)
+        checker = checker.sum(1)
+        
+        for i in range(len(checker)):
+            sh_im = self.image[i].shape  # Note the shape of the ith image
+            imorig = np.expand_dims(self.image[i], 0)
+            imorig_float = imorig * 255
+            lowimg = np.expand_dims(self.raw[i], 0)
+            
+            expanded_h = False
+            expanded_w = False
+
+            # Check and expand height if necessary
+            if sh_im[1] % 2 == 1:
+                expanded_h = True
+                imorig = np.concatenate((imorig, imorig[:, :, -1:, :]), axis=2)
+            
+            # Check and expand width if necessary
+            if sh_im[2] % 2 == 1:
+                expanded_w = True
+                imorig = np.concatenate((imorig, imorig[:, :, :, -1:]), axis=3)
+
+            # Convert to Tensor
+            imorig_tensor = torch.Tensor(imorig)
+
+            # Sets data type according to CPU or GPU modes
+            dtype = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+            # Noise level map
+            nsigma = (imorig_float - lowimg) / lowimg
+            nsigma = 0 + (np.max(nsigma) * 2 - 0) * (nsigma - np.min(nsigma)) / (np.max(nsigma) - np.min(nsigma))
+            nsigma = np.clip(nsigma, 0, 255) / 255
+
+            nsigma = nsigma.astype('float32')
+            nsigma_shape = (imorig_tensor.shape[2] // 2, imorig_tensor.shape[3] // 2)
+            nsigma = nsigma[:, :, :nsigma_shape[0], :nsigma_shape[1]]  # Ensure nsigma dimensions match expected shape
+
+            # Test mode
+            with torch.no_grad():
+                imorig_tensor = Variable(imorig_tensor.type(dtype))
+                nsigma_tensor = Variable(torch.FloatTensor(nsigma).type(dtype))
+
+            #print(f"imorig_tensor shape: {imorig_tensor.shape}, nsigma_tensor shape: {nsigma_tensor.shape}")
+            #print(f"nsigma_pixel value range: {nsigma_tensor.min()} - {nsigma_tensor.max()}")
+
+            # Estimate noise and subtract it from the input image
+            im_noise_estim = self.net(imorig_tensor, nsigma_tensor)
+            outim = torch.clamp(imorig_tensor - im_noise_estim, 0., 1.)
+
+            outim_np = outim.cpu().detach().numpy()
+
+            # Remove expanded parts to keep the original shape
+            if expanded_h:
+                outim_np = outim_np[:, :, :-1, :]
+            if expanded_w:
+                outim_np = outim_np[:, :, :, :-1]
+
+            # Store the processed image back
+            self.image[i] = outim_np.squeeze(0)  # Remove the extra dimension added by expand_dims
